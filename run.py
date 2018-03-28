91bf96005bbe5c127221bd0ccc6c8a7bdd64e4a3
niklsspb <prof051@yandex.ru>
2018-03-28 16:37:06 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,55 @@
+import logging
+import sys
+
+from telegram.ext import *
+
+from jobs import start, get_gold, get_everyday
+from actions import get_my_orders
+from configs import *
+
+
+# logging.basicConfig(
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     level=logging.DEBUG)
+# logger = logging.getLogger(__name__)
+
+
+
+def main():
+    updater = Updater(bot_token)
+    jobs = updater.job_queue
+    #jobs.run_repeating(sender_to_channel, 60)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("getgold", get_gold))
+    dp.add_handler(CommandHandler("everyday", get_everyday))
+    dp.add_handler(MessageHandler([Filters.text], get_my_orders))
+    dp.add_handler(MessageHandler([Filters.group], get_my_orders))
+    # dp.add_handler(CallbackQueryHandler(router_callbacks))
+    updater.start_polling()
+    updater.idle()
+
+
+def main(token):
+    updater = Updater(token)
+    jobs = updater.job_queue
+    #jobs.run_repeating(sender_to_channel, 60)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("getgold", get_gold))
+    dp.add_handler(CommandHandler("everyday", get_everyday))
+    dp.add_handler(MessageHandler([Filters.text], get_my_orders))
+    dp.add_handler(MessageHandler([Filters.group], get_my_orders))
+    # dp.add_handler(CallbackQueryHandler(router_callbacks))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        pass
+        # main()
+
+    #print(sys.argv[0])
+    # main(sys.argv[0])
